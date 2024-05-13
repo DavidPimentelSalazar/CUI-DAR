e6c0643b402fbfa35c6c8ba26a51cc7b574edddc
David Pimentel <daviidpiimentel@gmail.com>
2024-05-13 10:44:08 +0200

    Login

---
+++ app/src/main/java/com/example/cuidar/Login.java
@@ -0,0 +1,47 @@
+package com.example.cuidar;
+
+
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    private ArrayList<EditText> editTexts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editTexts = new ArrayList<>();
+        editTexts.add(findViewById(R.id.userEditTextLogin));
+        editTexts.add(findViewById(R.id.passwdEditTextLogin));
+
+        Button loginBtn = findViewById(R.id.btnLogin);
+        loginBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String username = editTexts.get(0).getText().toString().trim();
+        String password = editTexts.get(1).getText().toString().trim();
+
+        if (username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please provide a username and password.", Toast.LENGTH_SHORT).show();
+        } else if (username.equals("admin") && password.equals("admin")) {
+            Intent it = new Intent(this, Main.class);
+            startActivity(it);
+            Toast.makeText(getApplicationContext(), "Logged in", Toast.LENGTH_SHORT).show();
+            finish();
+        } else {
+            Toast.makeText(getApplicationContext(), "Invalid username or password.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cuidar/Main.java
@@ -0,0 +1,19 @@
+package com.example.cuidar;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
